d2878e74ce04ca03d8dd21961e63cef28ec06125
Sven Erik Knop <seknop@gmail.com>
2023-01-24 10:41:40 +0000

    Need to GRANT USAGE to SCHEMA as well.

--- movie_datagen/db/postgres_template.sql
+++ movie_datagen/db/postgres_template.sql
@@ -1,3 +1,3 @@
 CREATE USER "{{user}}" WITH PASSWORD '{{user}}-secret' NOCREATEDB;
-GRANT SELECT ON ALL TABLES IN SCHEMA public to "{{user}}";
-
+GRANT SELECT ON ALL TABLES IN SCHEMA movielens to {{user}};
+GRANT USAGE ON SCHEMA movielens TO {{user}};
