5036d9a14b03d5c252f3f7de617d45018d0e8087
Sven Erik Knop <seknop@gmail.com>
2023-08-03 11:00:35 +0100

    Wrong port definition

--- terraform/ccloud_security_group.tf
+++ terraform/ccloud_security_group.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "confluent-cloud" {
   ingress {
     description = "All TCP traffic"
     from_port = 0
-    to_port = 0
+    to_port = 65535
     protocol = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
